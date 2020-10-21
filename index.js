3e3cfe2c363a9338b3a1f953f62a27e25e8633f3
JakeMaz <jakemazanowski@gmail.com>
2020-10-21 12:01:08 -0400

    added ID numbers

--- index.js
+++ index.js
@@ -12,28 +12,30 @@ fetch('https://pokeapi.co/api/v2/pokemon')
         let pokeName = name.charAt(0).toUpperCase() + name.slice(1);
         // console.log(pokeName)
         let pokemon = document.createElement('li');
-        //! let pokeIdNum = document.createElement('p');
+        let pokeIdNum = document.createElement('p');
         let pokeType = document.createElement('p');
+        pokeType.className = 'pType';
+        pokeIdNum.className = 'pId';
         pokemon.innerHTML = pokeName;
         pokemonList.appendChild(pokemon);
-        //! pokemon.appendChild(pokeIdNum);
+        pokemon.appendChild(pokeIdNum);
         pokemon.appendChild(pokeType);
         // for(t = 1; t <= 9; t++)
         // console.log
         // console.log(p)
-        let t = p + 1;
-        let location = p +1;
+        let i = p + 1;
         // console.log(t)
-        if(t === p + 1){
-            fetch(`${baseURl}${t}`)
+        if(i === p + 1){
+            fetch(`${baseURl}${i}`)
             .then(function(pokeIndex){
                 return pokeIndex.json()
             })
             .then(function(json){
                 // console.log(json.id);
                 let mainType = json.types[0].type.name;
-                //! let IdNum = json.id;
+                let pIdNum = json.id;
                 pokeType.innerText = `Main Type: ${mainType.toUpperCase()}`;
+                pokeIdNum.innerText = `ID: ${pIdNum}`;
             })
         } 
     }
