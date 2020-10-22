ab227dc522ea775545f4d8137fdbc5812943dbeb
JakeMaz <jakemazanowski@gmail.com>
2020-10-22 13:24:51 -0400

    random gen works and shows option selected

--- index.js
+++ index.js
@@ -8,34 +8,46 @@ fetch(`${baseURL}${p}`)
     return response.json();
 })
 .then(function(json){
-    // console.log(json);
+    // console.log(json.sprites.front_default);
     let pokeIdNum = json.id
     let pokeName = json.name;
     let pokeType = json.types[0].type.name;
-
-    // let pokeRPSopt = json.name;
+    let pokeIcon = json.sprites.front_default;
 
     let pokemonName = pokeName.charAt(0).toUpperCase()+pokeName.slice(1);
 
     let pokemon = document.createElement('li');
     let pokemonType = document.createElement('p');
     let pokemonIdNum = document.createElement('p');
+    
 
     let pokemonRPS = document.getElementById('pokemonChoise')
-
     let pokemonRPSopt = document.createElement('option');
 
     pokemon.innerText = pokemonName;
     pokemonType.innerText = `Main Type: ${pokeType.toUpperCase()}`;
     pokemonIdNum.innerText = `ID: #${pokeIdNum}`;
-    
+
     pokemonRPSopt.innerText = `${pokeName.toUpperCase()}!`;
 
     pokemonList.appendChild(pokemon);
+    
     pokemon.appendChild(pokemonIdNum);
     pokemon.appendChild(pokemonType);
 
     pokemonRPS.appendChild(pokemonRPSopt);
+
+    pokemon.style.listStyleImage = `url(${pokeIcon})`;
+    
+    if(pokeType === 'grass'){
+        pokemon.style.color = 'green';
+    } else if (pokeType === 'fire'){
+        pokemon.style.color = 'red';
+    } else if (pokeType === 'water'){
+        pokemon.style.color = 'blue';
+    } else {
+        pokemon.style.cololr = 'black';
+    }
 
 })
 }
@@ -58,11 +70,32 @@ function randomPokemon(e) {
         return randomResults.json();
     })
     .then(function(randomJson){
-        console.log(randomJson.name)
-        let randomName = document.createElement('h2')
+        
+        let randomIcon = document.createElement('img');
+        let randomName = document.createElement('h2');
+        let randomType = document.createElement('p');
+        let myChoise = document.getElementById('myChoise');
+        let pokemonChoise = document.getElementById('pokemonChoise');
 
+        let rType = randomJson.types[0].type.name;
+
+        randomIcon.src = randomJson.sprites.front_default;
         randomName.textContent = randomJson.name.toUpperCase();
+        randomType.textContent = randomJson.types[0].type.name;
 
+        myChoise.innerHTML = pokemonChoise.options[pokemonChoise.selectedIndex].text;
+        randomPoke.appendChild(randomIcon)
         randomPoke.appendChild(randomName);
+        randomPoke.appendChild(randomType);
+
+        if(rType === 'grass'){
+            randomPoke.style.color = 'green';
+        } else if (rType === 'fire'){
+            randomPoke.style.color = 'red';
+        } else if (rType === 'water'){
+            randomPoke.style.color = 'blue';
+        } else {
+            randomPoke.style.cololr = 'black';
+        }
     })
 }
